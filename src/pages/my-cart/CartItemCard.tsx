0559f190ca8f707581c5da7f21c483948ab56147
pseudopunit <punit.choudhary14@gmail.com>
2024-05-05 14:40:29 +0530

    Updated cart item card

--- src/pages/my-cart/CartItemCard.tsx
+++ src/pages/my-cart/CartItemCard.tsx
@@ -3,7 +3,7 @@ import { Box, IconButton, Typography } from "@mui/material";
 import Image from "next/image";
 import styles from "@/pages/my-cart/CartItemCard.module.css";
 import { Cart } from "@/types/Cart";
-import { Add, Delete, DeleteOutline, Remove } from "@mui/icons-material";
+import { Add, DeleteOutline, Remove } from "@mui/icons-material";
 
 interface Props {
   cart: Cart;
@@ -16,8 +16,8 @@ const CartItemCard = ({ cart, onAdd, onRemove }: Props) => {
     <Box className={styles.container}>
       <Box className={styles.image}>
         <Image
-          src={cart.product.image}
-          alt={cart.product.productName}
+          src={cart.product?.image}
+          alt={cart.product?.productName}
           layout="fill"
           objectFit="contain"
         />
@@ -25,14 +25,14 @@ const CartItemCard = ({ cart, onAdd, onRemove }: Props) => {
       <Box className={styles.textAndButton}>
         <Box className={styles.textContainer}>
           <Typography className={styles.title}>
-            {cart.product.productName}
+            {cart.product?.productName}
           </Typography>
           <Typography className={styles.description}>
-            {cart.product.productDescription}
+            {cart.product?.productDescription}
           </Typography>
           <Typography
             className={styles.price}
-          >{`Rs. ${cart.product.sellingPrice}`}</Typography>
+          >{`Rs. ${cart.product?.sellingPrice}`}</Typography>
         </Box>
         <Box className={styles.addRemoveButtonContainer}>
           <IconButton onClick={onRemove}>
